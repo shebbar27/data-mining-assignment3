7c82650b6bb80ff7ce298880aa10bc0b8eec7a3f
Sunaada Hebbar <bhland@Sunaadas-Air.lan>
2022-11-30 13:10:10 -0700

    organize data set

--- classification.py
+++ classification.py
@@ -2,19 +2,14 @@ import csv
 import cv2
 import numpy as np
 import os
-import pandas as pd
 import pickle
-
-from skimage.transform import resize
-from sklearn import svm
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
+import shutil
 
 
 TEST_DIR = 'testPatient/'
 TEST_LABELS_FILE = 'test_Labels.csv'
 TRAIN_DIR = 'PatientData/'
+LABELLED_DATA_DIR = TRAIN_DIR + 'LabelledData/'
 IMAGE_EXTENSION = '.png'
 IMAGE_FILE_SUFFIX = 'thresh' + IMAGE_EXTENSION
 LABEL_FILE_EXTENSION = '.csv'
@@ -22,7 +17,6 @@ LABEL_FILE_SUFFIX = 'Labels' + LABEL_FILE_EXTENSION
 FILE_NAME_SEPERATOR = '_'
 IC_HEADER = 'IC'
 LABEL_HEADER = 'Label'
-NAME_HEADER = 'Name'
 MODEL_FILE_NAME = 'svm_model.sav'
 
 
@@ -36,6 +30,13 @@ def join_path(dir, filename):
     return os.path.join(dir, filename)
 
 
+# utlity function to clear all contents of given directory
+def init_empty_dirs(dir_path):
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path, ignore_errors=True)
+    os.makedirs(dir_path)
+
+
 # utility function to write data to csv file
 def write_to_csv_file(file_path, header, rows):
     with open(file_path, 'w') as file:
@@ -45,44 +46,41 @@ def write_to_csv_file(file_path, header, rows):
 
 
 # utility function to read data from csv file
-def read_from_csv_file(dir, filename):
-    csvDict = {}
-    with open(join_path(dir, filename), 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            key = filename
-            key = key.removesuffix(LABEL_FILE_SUFFIX) + IC_HEADER + FILE_NAME_SEPERATOR + row[IC_HEADER] + FILE_NAME_SEPERATOR + remove_file_extension(IMAGE_FILE_SUFFIX)
-            csvDict[key] = 1 if row[LABEL_HEADER] != '0' else 0
-    return csvDict
+def read_from_csv_file(file_path):
+    csv_dict = {}
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                key = IC_HEADER + FILE_NAME_SEPERATOR + row[IC_HEADER] + FILE_NAME_SEPERATOR + remove_file_extension(IMAGE_FILE_SUFFIX)
+                csv_dict[key] = 1 if row[LABEL_HEADER] != '0' else 0
+    else:
+        print(f'Error file: {file_path} does not exist!')
+    return csv_dict
 
 
 # function to read all the brain images form all the sub directories under the given directory
-def read_image_data(image_dir):
-    print("Reading image data")
-    images = {}
+# and move them to class folder based on labels
+def read_and_organize_image_data(image_dir):
+    print("Reading and oragnizing image data with labels")
+    init_empty_dirs(join_path(LABELLED_DATA_DIR, '0/'))
+    init_empty_dirs(join_path(LABELLED_DATA_DIR, '1/'))
 
     # get list of all sub directories under Slices folder    
-    image_dirs = [dir for dir in os.listdir(image_dir) if os.path.isdir(join_path(image_dir, dir))]
+    image_dirs = [dir for dir in os.listdir(image_dir) if (os.path.isdir(join_path(image_dir, dir)) and not LABELLED_DATA_DIR.__contains__(dir))]
 
     for dir in image_dirs:
         sub_dir_path = join_path(image_dir, dir)
-        for file_name in os.listdir(sub_dir_path):
-            if file_name.endswith(IMAGE_FILE_SUFFIX):
-                brain_image = cv2.imread(join_path(sub_dir_path, file_name))
-                key = dir + FILE_NAME_SEPERATOR + remove_file_extension(file_name)
-                images[key] = brain_image
-    return images
-
-
-# function to read lables from the given directory
-def read_data_labels(labels_dir):
-    print("Reading label data")
-    data_labels = []
-    for file_name in os.listdir(labels_dir):
-        if file_name.endswith(LABEL_FILE_EXTENSION):
-            csvDict = read_from_csv_file(labels_dir, file_name)
-            data_labels.extend(list(csvDict.items()))
-    return data_labels
+        labels_dict = read_from_csv_file(join_path(image_dir, dir + FILE_NAME_SEPERATOR + LABEL_FILE_SUFFIX))
+        if len(labels_dict) == 0:
+            print("Error labels not found!")
+        else:
+            for file_name in os.listdir(sub_dir_path):
+                if file_name.endswith(IMAGE_FILE_SUFFIX):
+                    label = str(labels_dict[remove_file_extension(file_name)])
+                    new_file_name = dir + FILE_NAME_SEPERATOR + file_name
+                    shutil.copy(join_path(sub_dir_path, file_name), join_path(LABELLED_DATA_DIR + label, new_file_name))
+    print("Labelled training data ready")
 
 
 # function to save model
@@ -91,87 +89,9 @@ def save_model(model, model_filename):
     pickle.dump(model, open(model_filename, 'wb'))
 
 
-# function to combime images tuple list with labels dictionary into dataframe
-def combine_images_and_labels(images, labels):
-    images_data_arr = []
-    label_data_arr = []
-    keys_arr = []
-    count = 0
-    h = 0
-    w = 0
-    print("Combining data into DataFrame")
-    for item in labels:
-        key = item[0]
-        label = item[1]
-        if key in images:
-            image = images[key]
-            if count == 0:
-                h, w, _ = image.shape
-                # print(h, w)
-                count += 1
-            else:
-                h_i, w_i, _ = image.shape
-                if h_i != h or w_i != w:
-                    image = resize(image, (h, w, 3))
-                    # print(h_i, w_i)
-        
-            images_data_arr.append(image.flatten())
-            label_data_arr.append(label)
-            keys_arr.append(key)
-
-    images_features = np.array(images_data_arr)
-    labels_col = np.array(label_data_arr)
-    df = pd.DataFrame(images_features)
-    df[LABEL_HEADER] = labels_col
-    df[NAME_HEADER] = keys_arr
-
-    # print(df.head(n=5))
-    # print(df.info())
-    print("DataFrame created sucessfully")
-    return df
-
-
-# function to get SVM model
-def get_svm_model(enable_probability=True):
-    param_grid = {
-                    'C': [0.1, 1, 10, 100], 
-                    'gamma': [0.0001, 0.001, 0.1, 1],
-                    'kernel': ['rbf', 'poly']
-                }
-
-    svc = svm.SVC(probability=enable_probability)
-    model = GridSearchCV(svc, param_grid, n_jobs=-1, verbose=5)
-    return model
-
-
 def main():
-    images = read_image_data(TRAIN_DIR)
-    labels = read_data_labels(TRAIN_DIR)
-    df = combine_images_and_labels(images, labels)
-    labelled_df = df.copy()
-    labelled_df.drop(NAME_HEADER, axis=1)
-
-    # supervised machine learning using SVM model
-    x=labelled_df.iloc[:,:-1]
-    y=labelled_df.iloc[:,-1]
-    model = get_svm_model()
+    read_and_organize_image_data(TRAIN_DIR)
     
-    print("Generating training and testing dataset")
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42, stratify=y)
-    
-    print("Training model started")
-    model.fit(x_train, y_train)
-    print("Training model started")
-
-    print("Evaluating model started")
-    y_pred = model.predict(x_test)
-    print("Evaluating model completed")
-
-    print("Model Performance: ")
-    print(f"The model is {accuracy_score(y_pred,y_test)*100}% accurate")
-
-    save_model(model, MODEL_FILE_NAME)
-
 
 if __name__ == '__main__':
     main()
